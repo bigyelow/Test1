2aea2ea30634b40622820c563aaa97a934df60cc
huangduyu <huangduyu@douban.com>
2017-07-05 16:28:16 +0800

    Add ut for String.urlEncoded

--- Test1/TestSwift.swift
+++ Test1/TestSwift.swift
@@ -11,6 +11,8 @@ import UIKit
 class TestSwift: NSObject {
   func test() {
     // 没有中文字符
+    print("".urlEncoded == "")
+    print(" ".urlEncoded == " ")
     print("adfasd".urlEncoded == "adfasd")
     print("http".urlEncoded == "http")
     print("http://".urlEncoded == "http://")
@@ -46,6 +48,5 @@ class TestSwift: NSObject {
     print("http://测试:1232dewef[];'@电影.com/path1/小电影?type=1&时间=很多&redir=douban://douban.com/世界很大?类型=没有#哈哈".urlEncoded == "http://%E6%B5%8B%E8%AF%95:1232dewef%5B%5D;'@%E7%94%B5%E5%BD%B1.com/path1/%E5%B0%8F%E7%94%B5%E5%BD%B1?type=1&%E6%97%B6%E9%97%B4=%E5%BE%88%E5%A4%9A&redir=douban://douban.com/%E4%B8%96%E7%95%8C%E5%BE%88%E5%A4%A7?%E7%B1%BB%E5%9E%8B=%E6%B2%A1%E6%9C%89#%E5%93%88%E5%93%88")
     print("http://测试:1232dewef[];'@电影.com?type=1&时间=很多&redir=douban://douban.com/世界很大?类型=没有#哈哈".urlEncoded == "http://%E6%B5%8B%E8%AF%95:1232dewef%5B%5D;'@%E7%94%B5%E5%BD%B1.com?type=1&%E6%97%B6%E9%97%B4=%E5%BE%88%E5%A4%9A&redir=douban://douban.com/%E4%B8%96%E7%95%8C%E5%BE%88%E5%A4%A7?%E7%B1%BB%E5%9E%8B=%E6%B2%A1%E6%9C%89#%E5%93%88%E5%93%88")
     print("http://测试:1232dewef[];'@电影.com/?type=1&时间=很多&redir=douban://douban.com/世界很大?类型=没有#哈哈".urlEncoded == "http://%E6%B5%8B%E8%AF%95:1232dewef%5B%5D;'@%E7%94%B5%E5%BD%B1.com?type=1&%E6%97%B6%E9%97%B4=%E5%BE%88%E5%A4%9A&redir=douban://douban.com/%E4%B8%96%E7%95%8C%E5%BE%88%E5%A4%A7?%E7%B1%BB%E5%9E%8B=%E6%B2%A1%E6%9C%89#%E5%93%88%E5%93%88")
-
   }
 }
