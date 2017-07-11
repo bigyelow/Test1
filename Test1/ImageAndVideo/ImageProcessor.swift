8a314729df1eec92bc05253f0454edfd86a3c651
huangduyu <huangduyu@douban.com>
2017-07-11 12:21:42 +0800

    Image: add detectFace()

--- Test1/ImageAndVideo/ImageProcessor.swift
+++ Test1/ImageAndVideo/ImageProcessor.swift
@@ -7,4 +7,35 @@
 //
 
 import Foundation
+import CoreImage
+import CoreGraphics
+import UIKit
+import Vision
 
+@available(iOS 11, *)
+class ImageProcessor {
+  static func detectFace(image: UIImage, completion: @escaping (CGRect?) -> Void) {
+    let request = VNDetectFaceRectanglesRequest { (request, error) in
+      guard let results = request.results as? [VNFaceObservation], let topResult = results.first else {
+        assert(false)
+        completion(nil)
+      }
+      print("confidence = \(topResult.confidence), rect = \(topResult.boundingBox))")
+      completion(topResult.boundingBox)
+    }
+
+    guard let cgImage = createCGImage(from: image) else { return }
+    let handler = VNImageRequestHandler(cgImage: cgImage)
+    do {
+      try handler.perform([request])
+    } catch {
+      print(error)
+    }
+  }
+
+  static private func createCGImage(from image: UIImage) -> CGImage? {
+    guard let ciImage = CIImage(image: image) else { return nil }
+    let context = CIContext(options: nil)
+    return context.createCGImage(ciImage, from: ciImage.extent)
+  }
+}
