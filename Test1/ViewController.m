b0227cf0e2d97813eabe37cbf28da08abcb2f2c7
huangduyu <huangduyu@douban.com>
2017-12-13 21:23:40 +0800

    addjust openURL

--- Test1/ViewController.m
+++ Test1/ViewController.m
@@ -55,8 +55,13 @@ static NSInteger OpenURLCount = 0;
 {
   if (self = [super init]) {
     _demos = @[WKWebViewStr, ImageStr, VideoStr, NSURLSessionStr, URLEncodingStr, PresentingStr, Nullability, OpenURL, Block, Macro, AuthenticationSession, CommonTest];
-    _appURLs = @[@"weixin://weixin.com/", @"letvclient://letvclient.com/xxx", @"pptv://pptv.com/ddd", @"sohuvideo://xxx.com/", @"iqiyi://iqiyi.com/", @"pptv://page/player/halfscreen?type=vod&vid=17083193&sid=17083193",
+    _appURLs = @[@"weixin://weixin.com/",
+                 @"letvclient://letvclient.com/xxx",
+                 @"pptv://pptv.com/ddd", @"sohuvideo://xxx.com/",
+                 @"iqiyi://iqiyi.com/", @"pptv://page/player/halfscreen?type=vod&vid=17083193&sid=17083193",
                  @"http://link-jump.youku.com/a/b/?spm=a2hmv.20009921.m_86987.5~5~5~5~5~5~5~A&action=play&vid=755070962&source=yksmartbanner_player_1&ua=Mozilla%2F5.0%20(iPhone%3B%20CPU%20iPhone%20OS%2010_0%20like%20Mac%20OS%20X)%20AppleWebKit%2F602.1.38%20(KHTML%2C%20like%20Gecko)%20Version%2F10.0%20Mobile%2F14A300%20Safari%2F602.1&ccts=1505447052960&cookieid=1505446352536Um0qvK%7Cwve5hf&fua=safari&special=1&ts=1505447052962&position=yksmartbanner_player_1"];
+    _appURLs = @[@"douban://douban.com/webview?url=https://www.douban.com/doubanapp/dispatch?uri=/skynet/46283721%26fallback%3dhttps%3a%2f%2fm.douban.com%2fpage%2fouy7i2r+",
+                 @"douban://douban.com/webview?url=https://www.douban.com/doubanapp/dispatch?uri=/skyt/46283721%26fallback%3dhttps%3a%2f%2fm.douban.com%2fpage%2fouy7i2r+"];
     OpenURLCount = _appURLs.count - 1;
     _becomeActiveCount = -1;
     _blockObject = [TestBlockObject new];
@@ -199,15 +204,10 @@ static NSInteger OpenURLCount = 0;
     [self _te_testAuthenticationSession];
   }
   else if ([_demos[indexPath.row] isEqualToString:CommonTest]) {
-//    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-//    dict[@"string"] = nil;
-//
-//    NSLog(dict[@"string"]);
-//
-    NSURL *url = [NSURL new];
-    if (url) {
-      NSLog(@"url not nil");
-    }
+    NSURLComponents *comp = [NSURLComponents componentsWithString:@"douban://douban.com/webview"];
+    NSString *query = [@"url=https://www.douban.com/doubanapp/dispatch?uri=/skyt/46283721&fallback=https://movie.douban.com" stringByAddingPercentEncodingWithAllowedCharacters: [NSCharacterSet URLQueryAllowedCharacterSet]];
+    comp.query = query;
+    NSLog(comp.URL.absoluteString);
   }
 }
 
