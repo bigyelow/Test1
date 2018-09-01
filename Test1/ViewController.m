b979083d5f994180936ec9dec2337a089115b27e
huangduyu <huangduyu@douban.com>
2018-09-01 17:10:08 +0800

    update URLSession demo

--- Test1/ViewController.m
+++ Test1/ViewController.m
@@ -42,7 +42,7 @@ static NSString * const MTURLProtocol = @"MTURLProtocol";
 static NSString * const TestDistance = @"TestDistance";
 static NSInteger OpenURLCount = 0;
 
-@interface ViewController () <NSURLSessionDelegate, UITableViewDelegate, UITableViewDataSource, WKNavigationDelegate>
+@interface ViewController () <NSURLSessionTaskDelegate, NSURLSessionDataDelegate, UITableViewDelegate, UITableViewDataSource, WKNavigationDelegate>
 
 @property (nonatomic, copy) NSArray *demos;
 @property (nonatomic, strong) WKWebView *webView;
@@ -316,18 +316,18 @@ static NSInteger OpenURLCount = 0;
   configuration.HTTPAdditionalHeaders = @{@"User-Agent": @"api-client/0.1.3 com.douban.frodo/4.9.0 iOS/10.2 x86_64"};
   NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
 
-  NSString *string1 = @"https://211.147.4.40/api/v2/group/yinxiangbiji?apikey=0dad551ec0f84ed02907ff5c42e8ec70&alt=json&douban_udid=e42a26fba5a1b560a4e6a465bb033e7ea402e4ff&event_loc_id=108288&latitude=0&loc_id=108288&longitude=0&udid=77cbb9dd272e97162d19281f92978a0049768e11&version=4.9.0";
-  NSString *string2 = @"https://frodo.douban.com/api/v2/movie/26873826?alt=json&apikey=0b8257e8bcbc63f4228707ba36352bdc&douban_udid=e42a26fba5a1b560a4e6a465bb033e7ea402e4ff&event_loc_id=108288&latitude=0&loc_id=108288&longitude=0&udid=77cbb9dd272e97162d19281f92978a0049768e11&version=4.9.0";
-  NSURL *url = [NSURL URLWithString:string1];
-  NSURLSessionDataTask *task = [session dataTaskWithURL:url
-                                      completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-                                        if (error) {
-                                          NSLog(@"error");
-                                        }
-                                        else {
-                                          NSLog(@"data = %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-                                        }
-                                      }];
+  NSString *urlStr = @"https://frodo.douban.com/api/v2/movie/26873826?alt=json&apikey=0b8257e8bcbc63f4228707ba36352bdc&douban_udid=e42a26fba5a1b560a4e6a465bb033e7ea402e4ff&event_loc_id=108288&latitude=0&loc_id=108288&longitude=0&udid=77cbb9dd272e97162d19281f92978a0049768e11&version=4.9.0";
+  NSURL *url = [NSURL URLWithString:urlStr];
+  NSURLSessionDataTask *task = [session dataTaskWithURL:url];
+//  NSURLSessionDataTask *task = [session dataTaskWithURL:url
+//                                      completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//                                        if (error) {
+//                                          NSLog(@"error");
+//                                        }
+//                                        else {
+//                                          NSLog(@"data = %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+//                                        }
+//                                      }];
   [task resume];
 }
 
@@ -414,11 +414,23 @@ static NSInteger OpenURLCount = 0;
 
 #pragma mark - URLSession delegate
 
-- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
- completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
 {
-  completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
+  NSLog(@"data = %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
 }
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+  if (!error) {
+    NSLog(@"no error");
+  }
+}
+
+//- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+// completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler
+//{
+//  completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
+//}
 
 #pragma mark - WKWebView delegate
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
